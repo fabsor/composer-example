7ec77488bd697ae8f06044dc1ead585c159d760d
Fabian Sorqvist <fabian@sorqvist.nu>
2015-05-22 15:30:42 +0200

    Test command for the next task

---
+++ src/OrmCommand.php
@@ -0,0 +1,81 @@
+<?php
+namespace Fabsor;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use GuzzleHttp\Client;
+
+class OrmCommand extends Command {
+  protected function configure() {
+    $this->setName('test-orm')
+      ->setDescription('Test a recipe web service')
+      ->addArgument('url', InputArgument::REQUIRED, 'The url to the web service');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $url = $input->getArgument('url');
+    if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+      throw new Exception('Enter a valid base URL');
+    }
+    $client = new Client();
+    $output->writeln('<info>Testing posting recipes...</info>');
+    $data = $this->testPost($client, $url . '/recipes');
+    $this->testGet($client, $url . '/recipes', $data['id']);
+    $output->writeln('<info>I Found my recipe!</info>');
+    $output->writeln('<info>Yay, you made it!</info>');
+  }
+
+  protected function testGet($client, $url, $id) {
+    $res = $client->get($url, array('headers' => array('Accept' => 'application/json')));
+    $this->assert(
+      $res->getStatusCode() == 200,
+      'The web service didn\'t return the right response code, should be 200. got ' . $res->getStatusCode());
+    $this->assert(
+      strpos($res->getHeader('content-type'), 'application/json') !== false,
+      'The web service didn\'t tell me it returned json');
+    $data = $res->json();
+
+    $this->assert(is_array($data), 'The data returned is not an array');
+    $this->assert(count($data) > 0, 'There should be at least one recipe here');
+    $this->assert(!empty($this->findRecipe($data, $id)), 'The recipe you created does not exist');
+    $res = $client->get($url . '/' . $id, array('headers' => array('Accept' => 'application/json')));
+    $data = $res->json();
+    $this->assert($data['id'] == $id, 'Could not retreive single recipe');
+  }
+
+  protected function findRecipe($recipes, $id) {
+    return array_filter($recipes, function ($item) use ($id) {
+        return $item['id'] == $id;
+      }
+    );
+  }
+
+  protected function testPost($client, $url) {
+    $body = array(
+      'title' => 'Brownies',
+      'description' => 'Bake them however you like'
+    );
+    $body['title'] .= rand(0, 5000);
+    $res = $client->post($url, array('json' => $body));
+    $this->assert(
+      $res->getStatusCode() == 201,
+      'The web service didn\'t return the right response code, when posting, should be 201');
+    $this->assert(
+      strpos($res->getHeader('content-type'), 'application/json') !== false,
+      'The web service didn\'t tell me it returned json');
+    $data = $res->json();
+    $this->assert(is_array($data), 'The data returned is not an array');
+    $this->assert($data['title'] == $body['title'], 'The title of the recipe is wrong');
+    $this->assert($data['description'] == $body['description'], 'The description of the recipe is wrong.');
+    $this->assert(!empty($data['id']), 'There should be an id present');
+    return $data;
+  }
+
+  protected function assert($condition, $msg) {
+    if (!$condition) {
+      throw new \Exception($msg);
+    }
+  }
+}
--- test.php
+++ test.php
@@ -3,10 +3,11 @@ require_once('vendor/autoload.php');
 
 use Fabsor\LsCommand;
 use Fabsor\TestCommand;
-
+use Fabsor\OrmCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 $application->add(new TestCommand());
 $application->add(new LsCommand());
+$application->add(new OrmCommand());
 $application->run();
